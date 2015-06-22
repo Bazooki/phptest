42089776e9a8dcee66d2653ab40ddd7551f5c388
Chris van Vuuren <chris.vanvuuren@wechat.co.za>
2015-06-22 17:24:42 +0200

    My test for php

---
+++ dockertest.php
@@ -0,0 +1,8 @@
+<?php
+
+//echo  "<img src='http://s2.postimg.org/5pil3da79/correct.png'>";
+//echo    $_SERVER['SERVER_ADDR'];
+//echo    $_SERVER['HTTP_HOST'];
+//echo    getenv('SERVER_ADDR');
+
+echo 'a posse ad esse';
